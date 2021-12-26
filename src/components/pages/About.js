93c662aa273022e1fb288354b6c6a42d55e94a4d
extramarks <mohd.faiz@extramarks.com>
2021-12-26 21:08:21 +0530

    github finder

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>about this app</h1>
+            <p>this app is to search github user</p>
+        </Fragment>
+    )
+}
